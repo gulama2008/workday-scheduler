8ece85d64edb001c91c506aae30341175f41c935
Siyu Liu <siyu.java.web@gmail.com>
2022-12-09 13:02:40 +1100

    add current date in the header

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,24 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var currentDay = $("#currentDay");
+  var date = dayjs();
+  function getCurrentDate() {   
+    console.log(date);
+    if (date.format("D") == 1 || date.format("D") == 21 || date.format("D") == 31) {
+      date = date.format("dddd, MMMM D[st]");
+    } else if (date.format("D") == 2 || date.format("D") == 22) {
+      date = date.format("dddd, MMMM D[nd]");
+    } else if (date.format("D") == 3 || date.format("D") == 23) {
+      date = date.format("dddd, MMMM D[rd]");
+    } else {
+      date = date.format("dddd, MMMM D[th]");
+    }
+    return date;
+  }
+  
+  getCurrentDate();
+  currentDay.text(date);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
