cef5762af0c4b46f51ac1d598016e7c576c81067
Siyu Liu <siyu.java.web@gmail.com>
2022-12-10 16:28:12 +1100

    create functions for saving and loading data from local storage

--- Assets/script.js
+++ Assets/script.js
@@ -4,15 +4,17 @@
 $(function () {
   var currentDay = $("#currentDay");
   var timeBlock = $(".time-block");
+  var saveButton = $(".saveBtn");
+  var textArea = $("textarea");
   var date = dayjs();
   var currentHour = date.format("H");
+
+  // create a function of getting the current date and display it in the header
   function getCurrentDate() {
     var dateShow;
     if (
-      date.format("D") == 1 ||
-      date.format("D") == 21 ||
-      date.format("D") == 31
-    ) {
+      date.format("D") == 1 || date.format("D") == 21 || date.format("D") == 31)
+    {
       dateShow = date.format("dddd, MMMM D[st]");
     } else if (date.format("D") == 2 || date.format("D") == 22) {
       dateShow = date.format("dddd, MMMM D[nd]");
@@ -24,6 +26,7 @@ $(function () {
     currentDay.text(dateShow);
   }
 
+  // create a function of changing the color of time-blocks based on current hour
   function changeColorForTimeBlock() {
     for (i = 0; i < timeBlock.length; i++) {
       var timeBlockHour = $(timeBlock[i]).attr("id").split("-")[1];
@@ -37,8 +40,27 @@ $(function () {
     }
   }
 
+  // create a function to save contents when clicking save icon
+  saveButton.each(function () { 
+    $(this).on("click", function () { 
+      var key = $(this).parent().attr("id");
+      var text = $(this).siblings("textarea").val();
+      localStorage.setItem(key, text);
+    })
+  })
+
+  // create function to load contents to the time block from local storage
+  function getValueFromLocalStorage() { 
+    for (i = 0; i < timeBlock.length; i++) {
+      var text = localStorage.getItem($(timeBlock[i]).attr("id"));
+      $(timeBlock[i]).children().eq(1).val(text); 
+    }
+  }
+
   getCurrentDate();
   changeColorForTimeBlock();
+  getValueFromLocalStorage();
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
